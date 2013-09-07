af235bad9296d6f441928f48311683bfa8ecc45d
altmivip <altmivip@163.com>
2013-09-07 18:27:30 +0800

    optimize some file of wiki and original

--- html_baobao/application/views/admin/wikiAdd.php
+++ html_baobao/application/views/admin/wikiAdd.php
@@ -2,11 +2,54 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>网站后台管理-新增<?php echo $this->_info['name']?></title>
 <link rel="stylesheet" href="/adminStatic/css/common_new.css" type="text/css" />
 <script type="text/javascript" src="/adminStatic/js/jquery.js" ></script>
+</head>
+<body>
+<div id="man_zone">
+	<h5>&nbsp;&nbsp;◆ 新增<?php echo $this->_info['name']?>&nbsp;&raquo;&nbsp;<span style='color:red'><b>*</b></span> 代表必填项；</h5>
+	<?php echo form_open(site_url('admin/'.$this->_info['cls'].'/saveAdd'), array('class'=>"jnice","onsubmit"=>"return check_form()","enctype"=>"multipart/form-data"));?>
+  	<table width="96%" border="0" align="center"  cellpadding="3" cellspacing="1" class="table_style">
+    	<tr>
+      		<td class="left_title_1"><span style='color:red'>*</span>&nbsp;所属标签</td>
+      		<td>
+      			<select name="tag_id" id="tag_id" >
+                	<option value="0">-- 请选择 --</option>
+                    <?php if(!empty($tags)) {foreach($tags as $k=>$v) {?>
+                    <option value="<?php echo $v['id']?>" style="background-color:#FFC;"><?php echo $v['name']?></option>
+                    <?php }} ?>
+                </select>
+                <span style="color:red;" id="errorTag"></span>
+            </td>
+    	</tr>
+    	<tr>
+      		<td width="18%" class="left_title_2">关键词</td>
+      		<td width="82%"><input class="text_style" type="text" name="<?php echo $this->_info['cls']?>_keyword" id="<?php echo $this->_info['cls']?>_keyword" /> <span style="color:red;" id="errorKeyword"></span></td>
+    	</tr>
+    	<tr>
+      		<td width="18%" class="left_title_1">图片</td>
+      		<td width="82%"><input  type="file" name="<?php echo $this->_info['cls']?>_image" id="<?php echo $this->_info['cls']?>_image" /> </td>
+    	</tr>
+    	<tr>
+      		<td class="left_title_2">内容</td>
+      		<td> <?php echo $kindeditor;?> <span style="color:red;" id="errorContent"></span></td>
+
+    	</tr>
+        <tr>
+      		<td class="left_title_1">&nbsp;</td>
+      		<td><input type="submit" name="submit" value=" 提交 " />&nbsp;&nbsp;
+            <input type="button" name="back" value=" 返回 " onclick="window.location.href='<?php echo site_url('admin/'.$this->_info['cls'].'/showList/');?>'"/>
+            </td>
+    	</tr>
+  	</table>
+    </form>
+</div>
 <script type="text/javascript" src="/adminStatic/js/common.js" ></script>
-<title>网站后台管理-新增百科</title>
 <script type="text/javascript">
+//定义控制器信息
+var _info = {'cls':'<?php echo $this->_info['cls']?>', 'name':'<?php echo $this->_info['name']?>'};
+
 function check_form()
 {
     var tag_id = $("#tag_id").val();
@@ -28,48 +71,5 @@ function check_form()
 
 }
 </script>
-</head>
-
-<body>
-<div id="man_zone">
-	<h5>&nbsp;&nbsp;◆ 新增百科&nbsp;&raquo;&nbsp;<span style='color:red'><b>*</b></span> 代表必填项；</h5>
-	<?php echo form_open(site_url('admin/wiki/saveAdd'), array('class'=>"jnice","onsubmit"=>"return check_form()","enctype"=>"multipart/form-data"));?>
-  	<table width="96%" border="0" align="center"  cellpadding="3" cellspacing="1" class="table_style">
-    	<tr>
-      		<td class="left_title_1"><span style='color:red'>*</span>&nbsp;所属标签</td>
-      		<td>
-      			<select name="tag_id" id="tag_id" >
-                	<option value="0">-- 请选择 --</option>
-                    <?php if(!empty($tags)) {foreach($tags as $k=>$v) {?>
-                    <option value="<?php echo $v['id']?>" style="background-color:#FFC;"><?php echo $v['name']?></option>
-                    <?php }} ?>
-                </select>
-                <span style="color:red;" id="errorTag"></span>
-            </td>
-    	</tr>
-    	<tr>
-      		<td width="18%" class="left_title_2">百科关键词</td>
-      		<td width="82%"><input class="text_style" type="text" name="wiki_keyword" id="wiki_keyword" /> <span style="color:red;" id="errorKeyword"></span></td>
-    	</tr>
-    	<tr>
-      		<td width="18%" class="left_title_1">百科图片</td>
-      		<td width="82%"><input  type="file" name="wiki_image" id="wiki_image" /> </td>
-    	</tr>
-    	<tr>
-      		<td class="left_title_2">百科内容</td>
-      		<td> <?php echo $kindeditor;?> <span style="color:red;" id="errorContent"></span></td>
-
-    	</tr>
-        <tr>
-      		<td class="left_title_1">&nbsp;</td>
-      		<td><input type="submit" name="submit" value=" 提交 " />&nbsp;&nbsp;
-            <input type="reset" name="reset" value=" 重置 " />
-            </td>
-    	</tr>
-  	</table>
-    </form>
-    <br />
-    <br /><br />
-</div>
 </body>
 </html>
