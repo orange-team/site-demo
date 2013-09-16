6eeaa139376bcf24ea3deeb5bb5a42bf8ba8c1cd
altmivip <altmivip@163.com>
2013-09-16 23:28:18 +0800

    optimize the user.css

--- html_baobao/application/views/user_reg.php
+++ html_baobao/application/views/user_reg.php
@@ -1,41 +1,6 @@
 <?php $this->load->view('header')?>
 <div id="wraper">
-    <div class="site_route">
-        您的位置 : <a href="/mmxue/">妈妈学</a> > 用户注册
-    </div>
     <div class="content clearfloat">
-    <style>
-    .reg ,.reg h1{ font-size:20px;color:#ccc;}
-    .reg { float:left;display:inline-block;width:505px;height:auto;font-size:20px;color:#ccc;font-weight:bold;margin:70px 80px;}
-    .reg .field { float:left;width:305px;height:38px;margin:12px 0 0 0;border:2px solid #ccc;}
-    .reg .field label { float:left;width:36px;height:36px; }
-    .reg .input { float:left;width:260px;height:38px;font-size:16px;margin:0 0 0 10px;}
-    .reg .authcode { width:99%; }
-    .reg .authcode .field { width:200px; }
-    .reg .authcode .input { width:190px; }
-    .reg .authcode .img_authcode { display:inline-block;float:left;width:92px;height:40px;margin:12px 0 0 10px;background:#fcf;}
-    .reg .authcode .img_authcode img { display:inline-block;border:none;width:92px;height:40px;}
-    .reg .btn_blue { width:98px;height:36px; }
-    .reg .btn_orange { width:98px;height:36px; }
-    .btn { -moz-border-radius: 4px; -moz-box-shadow: inset 0 1px 0 rgba(255,255,255,.28),0 -1px 1px rgba(0,0,0,.28); -webkit-border-radius: 4px; -webkit-box-shadow: inset 0 1px 0 rgba(255,255,255,.28),inset 0 -1px 1px rgba(0,0,0,.28); background-color: #1bcdcf; border: 1px solid #00acad; border-color: rgba(0,0,0,.1)rgba(0,0,0,.1)rgba(0,0,0,.1)rgba(0,0,0,.1); border-radius: 4px; box-shadow: inset 0 1px 0 rgba(255,255,255,.28),inset 0 -1px 1px rgba(0,0,0,.28); color: #fff; cursor: pointer; display: inline-block; font-family: "Helvetica Neue",helvetica,arial,sans-serif; font-size: 18px; font-weight: 700; line-height: 1; margin: 0; padding: 8px 17px 11px; text-align: center; text-decoration: none; text-shadow: 0 1px 0 rgba(0,0,0,.2); vertical-align: middle; }
-    .reg .orange { background-color: #f37049;}
-    
-    .reg .form_submit {height:60px;line-height:75px;}
-    .reg .yes {border:2px solid #6c0;}
-    .reg .err {border:2px solid red;}
-    .reg .err_txt {float:left;display:inline-block;width:190px;height:42px;line-height:42px;margin:12px 0 0 5px;color:red;font-size:14px;font-weight:normal;}
-    .reg .authcode .err_txt { width:90px; }
-    .reg .field_protocal { margin:10px 0;font-size:14px;color:#333;font-weight:normal;height:40px;}
-    .reg .field_protocal a { text-decoration:none;color:#076699;}
-    /*其他登录方式*/
-    .other_reg { float:left;display:inline-block;margin:70px 0;padding:0 0 0  65px;border-left:1px solid #c8c8c8;width:205px;height:320px;font-size:20px;color:#585858; }
-    .other_reg dl img { float:left;display:inline-block;border:none;width:36px;height:36px;}
-    .other_reg dt { color:#666;font-weight:normal;margin:20px 0;}
-    .other_reg label {float:left;display:inline-block;height:36px;line-height:36px;margin:0 0 0 10px;}
-    .other_reg .to_reg { display:block;width:170;height:26px;margin:40px 0 0 0;padding:8px 0 0 0;font-weight:600;}
-    .other_reg a.to_reg:hover { color:#fff; }
-    .other_reg a.to_reg { color:#fff; }
-    </style>
         <div class="reg clearfloat">
         <?php echo form_open(site_url($this->_info['cls'].'/reg/'.urlencode($this->_ref), array('name'=>'reg',"onsubmit"=>"return chk_reg()")));?>
             <h1>注册蜡笔画</h1>
