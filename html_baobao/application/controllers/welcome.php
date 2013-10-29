bf8749996a5a7d420eadb25b05f625b2f3e16b41
arkulo56 <arkulo@126.com>
2013-10-29 19:38:01 +0800

    update module

--- html_baobao/application/controllers/welcome.php
+++ html_baobao/application/controllers/welcome.php
@@ -8,11 +8,32 @@ class Welcome extends MY_Controller
 		$this->lang->load('common');
 		$this->lang->load('index');
 	}
-	public function index($userid)
+	public function getwiki($userid)
 	{
         header("Content-type: text/html; charset=utf-8"); 
         $this->load->library("usermodel_tmp/umodel",array($userid));
         $res = $this->umodel->getWiki();
+        print_r($res);
+    }
+    public function getarticle($userid)
+    {
+        header("Content-type: text/html; charset=utf-8"); 
+        $this->load->library("usermodel_tmp/umodel",array($userid));
+        $res = $this->umodel->getArticle();
+        print_r($res);
+    }
+    public function getaskarticle($userid)
+    {
+        header("Content-type: text/html; charset=utf-8"); 
+        $this->load->library("usermodel_tmp/umodel",array($userid));
+        $res = $this->umodel->getAskArticle();
+        print_r($res); 
+    }
+    public function gettag($userid)
+    {
+        header("Content-type: text/html; charset=utf-8"); 
+        $this->load->library("usermodel_tmp/umodel",array($userid));
+        $res = $this->umodel->getTag();
         print_r($res);
     }
 
