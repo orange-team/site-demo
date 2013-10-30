453d047b8abb323b7805bb4717611ee343f62314
arkulo56 <arkulo@126.com>
2013-10-30 23:32:30 +0800

    Update Umodel.php
    add paras include tags

--- html_baobao/application/libraries/usermodel_tmp/Umodel.php
+++ html_baobao/application/libraries/usermodel_tmp/Umodel.php
@@ -12,12 +12,12 @@ class Umodel implements Ubase
     private $section = 0;
     private $userModel = "";
     private $userModelArr = array(1=>"Ucommon",2=>"Uprepare",3=>"Uprogprop",4=>"Uprogmedium",5=>"Uproglate",6=>"Uoneage",7=>"Uthreeage");  
-    public function __construct($user_id)
+    public function __construct($paras)
     {
         $this->CI = &get_instance();
         //查询当前用户状态
         $this->CI->load->model("user_model","user");
-        $userType = $this->CI->user->get($user_id[0]);
+        $userType = $this->CI->user->get($paras[0]);
         $this->section = $userType['section'];
         $userModelFile = $this->userModelArr[$this->section];
         //调用相应的用户模型
@@ -25,6 +25,11 @@ class Umodel implements Ubase
         $method = strtolower($userModelFile);
         $this->hd = & $this->CI->$method;        
         $this->hd->section = $this->section;
+        //tags sended into common object
+        if("Ucommon"==$userModelFile)
+        {
+            $this->hd->tags = $paras[1];
+        }
     }
 
     public function getAskArticle()
